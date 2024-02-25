51a64a526d357af366d4c69a6a7931ba5d65c467
Ravi <lcs2020043@iiitl.ac.in>
2024-02-25 14:45:03 +0530

    Delete page.js and add getMeal function

--- app/meals/[slug]/page.js
+++
@@ -1,9 +0,0 @@
-export default function Page({ params }) {
-  return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        {params.slug} Page
-      </h1>
-    </main>
-  );
-}
